59d1759576ff9fda81f53789c1ad7a1f5682decf
Juhani Leppänen <juhani.leppanen@protonmail.com>
2018-01-24 21:43:18 +0200

    Fix syntax

--- macos_sublime_setup.sh
+++ macos_sublime_setup.sh
@@ -8,19 +8,19 @@ mkdir -p "$HOME/Library/Application Support/Sublime Text 3/Packages"
 umask 0022
 
 # install package control
-if [ ! -f "$HOME/Library/Application Support/Sublime Text 3/Installed Packages/Package Control.sublime-package" ]; then
+if [ ! -f "$HOME/Library/Application Support/Sublime Text 3/Installed Packages/Package Control.sublime-package" ] ; then
   wget -P "$HOME/Library/Application Support/Sublime Text 3/Installed Packages" \
     "https://packagecontrol.io/Package%20Control.sublime-package"
 fi
 
 # create symbolic link
-if [ ! -h "$HOME/Library/Application Support/Sublime Text 3/Packages/User" ]; then
+if [ ! -h "$HOME/Library/Application Support/Sublime Text 3/Packages/User" ] ; then
   ln -s "$HOME/Development/personal/dotfiles/sublime" \
     "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
 fi
 
 # install or update base16 color schemes
-if [ ! -d "$HOME/Library/Application Support/Sublime Text 3/Packages/Base16" ]; then
+if [ ! -d "$HOME/Library/Application Support/Sublime Text 3/Packages/Base16" ] ; then
   cd "$HOME/Library/Application Support/Sublime Text 3/Packages"
   git clone git://github.com/chriskempson/base16-textmate.git Base16
 else
