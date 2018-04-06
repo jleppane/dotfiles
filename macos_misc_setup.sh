8dd5fb12865ace8f1646dcb72b154906e8902ec2
Juhani Leppänen <juhani.leppanen@protonmail.com>
2018-04-06 17:14:48 +0300

    Add .gitconfig, .hgrc and macos_misc_setup.sh

---
+++ macos_misc_setup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# create symbolic links
+ln -sf "$HOME/Development/personal/dotfiles/.gitconfig" "$HOME/.gitconfig"
+ln -sf "$HOME/Development/personal/dotfiles/.hgrc"      "$HOME/.hgrc"
