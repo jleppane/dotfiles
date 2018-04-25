97542ca2fcb9736f86ec4918f6238e10960f828f
Juhani Leppänen <juhani.leppanen@protonmail.com>
2018-04-25 23:47:49 +0300

    Rename $SCRIPT_PATH to $SCRIPTS_PATH

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,18 +3,18 @@
 DOTFILES_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && dirname "$PWD" )"
 
 DOTFILES_PATH="$DOTFILES_ROOT/dotfiles"
-SCRIPT_PATH="$DOTFILES_ROOT/script"
+SCRIPTS_PATH="$DOTFILES_ROOT/scripts"
 
 if [[ "$OSTYPE" == "darwin"* ]] ; then
 
-  . "$SCRIPT_PATH/macos_brew_install.sh"
-  . "$SCRIPT_PATH/macos_misc_install.sh"
+  . "$SCRIPTS_PATH/macos_brew_install.sh"
+  . "$SCRIPTS_PATH/macos_misc_install.sh"
 
-  . "$SCRIPT_PATH/macos_bash_setup.sh"
-  . "$SCRIPT_PATH/macos_iterm_setup.sh"
-  . "$SCRIPT_PATH/macos_misc_setup.sh"
-  . "$SCRIPT_PATH/macos_sublime_setup.sh"
-  . "$SCRIPT_PATH/macos_vim_setup.sh"
-  . "$SCRIPT_PATH/macos_virtualbox_setup.sh"
+  . "$SCRIPTS_PATH/macos_bash_setup.sh"
+  . "$SCRIPTS_PATH/macos_iterm_setup.sh"
+  . "$SCRIPTS_PATH/macos_misc_setup.sh"
+  . "$SCRIPTS_PATH/macos_sublime_setup.sh"
+  . "$SCRIPTS_PATH/macos_vim_setup.sh"
+  . "$SCRIPTS_PATH/macos_virtualbox_setup.sh"
 
 fi
--- scripts/macos_vim_setup.sh
+++ scripts/macos_vim_setup.sh
@@ -4,4 +4,4 @@
 ln -sf "$DOTFILES_PATH/.vimrc" "$HOME/.vimrc"
 
 # install plugins
-. "$SCRIPT_PATH/vim_install_plugins.sh"
+. "$SCRIPTS_PATH/vim_install_plugins.sh"
