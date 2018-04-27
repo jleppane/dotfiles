87f5b5614eec0303a951d277aaf42f08deb6ca94
Juhani Leppänen <juhani.leppanen@protonmail.com>
2018-04-27 20:36:42 +0300

    Remove "last login" message on terminal

--- scripts/macos_misc_setup.sh
+++ scripts/macos_misc_setup.sh
@@ -4,3 +4,8 @@
 ln -sf "$DOTFILES_PATH/.ackrc"     "$HOME/.ackrc"
 ln -sf "$DOTFILES_PATH/.gitconfig" "$HOME/.gitconfig"
 ln -sf "$DOTFILES_PATH/.hgrc"      "$HOME/.hgrc"
+
+# remove "last login" message on terminal
+if [ ! -f "$HOME/.hushlogin" ] ; then
+  touch "$HOME/.hushlogin"
+fi
