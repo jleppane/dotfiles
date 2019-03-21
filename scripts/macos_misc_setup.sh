4ddb56b32dcd06c3333d449f13910a328e20c4b7
Juhani Leppänen <juhani.leppanen@protonmail.com>
2019-03-21 04:35:15 +0200

    Add global .gitignore

--- scripts/macos_misc_setup.sh
+++ scripts/macos_misc_setup.sh
@@ -3,6 +3,7 @@
 # create symbolic links
 ln -sf "$DOTFILES_PATH/.ackrc"     "$HOME/.ackrc"
 ln -sf "$DOTFILES_PATH/.gitconfig" "$HOME/.gitconfig"
+ln -sf "$DOTFILES_PATH/.gitignore" "$HOME/.gitignore"
 ln -sf "$DOTFILES_PATH/.hgrc"      "$HOME/.hgrc"
 ln -sf "$DOTFILES_PATH/.htoprc"    "$HOME/.htoprc"
 
